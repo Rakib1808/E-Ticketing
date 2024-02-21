266c8931ceb14ab180f373311e4b131292b7b2e5
Rakib Hasan <rakibhasan1808@gmail.com>
2024-02-21 18:13:11 +0600

    update js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// function play(){
-//     //step-1: hide the home screen, to hide the screen add the class hiddine to home section
-//     const homeFirstPart = document.getElementById('1st-part')
-//     homeFirstPart.classList.add ('hidden');
-
-//     const homeSecondPart = document.getElementById ('footer')
-//     homeSecondPart.classList.add ('hidden')
-
-//     //show the Success page
-//     const secondPart = document.getElementById ('2nd-part')
-//     secondPart.classList.remove('hidden')
-// }
 
 function play(){
-    hideElementById ('1st-part')
-    hideElementById ('footer')
-    showElementById ('2nd-part')
+    hideElementById ('1st-part');
+    hideElementById ('footer');
+    showElementById ('2nd-part');
+    seatSelect();
 }
+
